9cc3499005f69db15824e857883f5724318c5d1c
sewonkimm <swon962@gmail.com>
2021-03-16 14:35:21 +0900

    feat: 🍎 vue-router 추가

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import '@/components/css/style.scss';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
