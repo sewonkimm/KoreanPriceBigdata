275b3ddb0f0f9c1f71a00b7f6080e62c1a4a76f3
sewonkimm <swon962@gmail.com>
2021-03-16 14:05:33 +0900

    feat: 🍎 vue2로 버전 다운그레이드
    - 라이브러리 생태계가 안정적이지 않아 vue3에서 vue2로 다운그레이드

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <div>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import './components/css/style.scss';
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-  },
-};
+    HelloWorld
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
