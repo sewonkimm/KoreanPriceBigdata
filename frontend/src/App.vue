725a288677af7a88d2f5280911b43c6acc46d089
sewonkimm <swon962@gmail.com>
2021-03-22 16:31:45 +0900

    feat: 🍎 vuetify 적용
    ✅ Closes: #S04P23A301

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Home />
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+import Home from './views/Home';
+
+export default {
+  name: 'App',
+
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
