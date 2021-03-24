42d21e9ed87f045d63a044ada85219ff5a90d879
sewonkimm <swon962@gmail.com>
2021-03-24 18:14:03 +0900

    feat: 🍎 메인페이지 레이아웃 markup
    - svg-loader 적용
    
    ✅ Closes: #S04P23A301-43

---
+++ frontend/src/views/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Main">
+    <div class="logo">
+      <Logo />
+    </div>
+
+    <v-toolbar class="search">
+      <v-text-field hide-details prepend-icon="mdi-magnify" single-line></v-text-field>
+    </v-toolbar>
+
+    <div class="cardContainer"></div>
+  </div>
+</template>
+<script>
+import '@/components/css/main/style.scss';
+import { Logo } from '@/assets/index.js';
+
+export default {
+  name: 'Main',
+  components: {
+    Logo,
+  },
+};
+</script>
