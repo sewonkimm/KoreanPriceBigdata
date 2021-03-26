e3dac021724643face424fd7e9b8e7c6f7f820b4
김세원 <swon962@gmail.com>
2021-03-26 00:20:53 +0000

    중간 발표를 위한 FE 배포

--- frontend/src/components/main/card.vue
+++ frontend/src/components/main/card.vue
@@ -1,32 +1,34 @@
 <template>
-  <div class="cardContainer">
+  <div class="card">
     <BookmarkGreen class="bookmark" />
     <img src="../../assets/warning.png" />
 
     <p class="name">
-      양파
+      {{ item.name }}
     </p>
     <div class="priceWrapper">
       <div class="priceTitle">
         현재가격
       </div>
-      <div class="price">
-        3,680원
-      </div>
+      <div class="price">{{ item.price }} 원</div>
     </div>
     <div class="num">
-      001
+      {{ index }}
     </div>
   </div>
 </template>
-
 <script>
 import '@/components/css/main/card.scss';
 import { BookmarkGreen } from '@/assets/index.js';
+
 export default {
   name: 'Card',
   components: {
     BookmarkGreen,
+  },
+  props: {
+    item: Object,
+    index: Number,
   },
 };
 </script>
