8cb9ffce94abbabe16b8f3301726a80610fc49d6
JeeyoungKim <jiiioiyoung96@gmail.com>
2021-03-29 15:58:17 +0900

    feat: 🍎 회원가입 페이지 마크업
    ✅ Closes: #s04p23a301-80

---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="register">
+    <div class="email" v-if="!isEmailCheck">
+      <EmailInput />
+      <button class="nextButton" v-on:click="isEmailCheck = !isEmailCheck">
+        다음
+      </button>
+    </div>
+    <div class="password" v-if="isEmailCheck">
+      <PasswordInput />
+    </div>
+  </div>
+</template>
+<script>
+import '@/components/css/register/style.scss';
+import EmailInput from '@/components/register/emailInput.vue';
+import PasswordInput from '@/components/register/passwordInput.vue';
+
+export default {
+  name: 'Register',
+  components: {
+    EmailInput,
+    PasswordInput,
+  },
+  data: function() {
+    return {
+      isEmailCheck: false,
+    };
+  },
+  methods: {},
+};
+</script>
