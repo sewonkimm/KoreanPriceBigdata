742b63e1f2d496748802d4bf0cb99f0c0f772f58
sewonkimm <swon962@gmail.com>
2021-03-29 16:39:59 +0900

    feat: 🍎 상세페이지 레이아웃
    ✅ Closes: #S04P23A301-79

---
+++ frontend/src/views/Detail.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="detail">
+    <!-- header -->
+    <v-toolbar color="primary">
+      <v-app-bar-nav-icon>
+        <v-btn icon>
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-app-bar-nav-icon>
+      <v-toolbar-title>{{ $route.params.id }}</v-toolbar-title>
+      <v-btn icon>
+        <v-icon>mdi-heart-outline</v-icon>
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar class="search" dense max-width="283px">
+        <v-text-field
+          hide-details
+          prepend-icon="mdi-magnify"
+          single-line
+          label="상품 검색"
+        ></v-text-field>
+      </v-toolbar>
+    </v-toolbar>
+
+    <!-- body -->
+    <v-container class="red lighten-5 justify-center align-center contents">
+      <v-row class="row">
+        <v-col :cols="3">
+          <v-card class="pa-2" outlined tile>
+            현재시세
+          </v-card>
+        </v-col>
+        <v-col :cols="9">
+          <v-card class="pa-2" outlined tile>
+            1년 가격 추이
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row class="row">
+        <v-col :cols="6">
+          <v-card class="pa-2" outlined tile>
+            쇼핑몰별 최저가
+          </v-card>
+        </v-col>
+        <v-col :cols="6">
+          <v-card class="pa-2" outlined tile>
+            추천시스템
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+import '@/components/css/detail/style.scss';
+
+export default {
+  name: 'Detail',
+};
+</script>
