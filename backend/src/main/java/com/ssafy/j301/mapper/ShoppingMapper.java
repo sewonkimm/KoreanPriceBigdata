f37d3d5785043370b2aabc1a920a813250012522
이은택 <rudus1012@naver.com>
2021-03-31 04:28:25 +0900

    feat: 🍎 ShoppingAp개발
    ✅ Closes: #S04P23A301-83

---
+++ backend/src/main/java/com/ssafy/j301/mapper/ShoppingMapper.java
@@ -0,0 +1,11 @@
+package com.ssafy.j301.mapper;
+
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import com.ssafy.j301.shopping.Shopping;
+
+@Repository
+public interface ShoppingMapper {
+
+	public List<Shopping> selectShopping(Long ingredientId);
+}
---
+++ backend/src/main/java/com/ssafy/j301/shopping/ShoppingController.java
@@ -0,0 +1,23 @@
+package com.ssafy.j301.shopping;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+
+@Api(tags = "Shoppings", description = "쇼핑 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/shoppings")
+public class ShoppingController {
+
+	private final ShoppingService shoppingService;
+
+	@GetMapping("/{ingredientId}")
+	public List<Shopping> selectShopping(@PathVariable Long ingredientId) {
+		return shoppingService.selectShopping(ingredientId);
+	}
+}
