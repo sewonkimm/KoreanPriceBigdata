6555e74e46c9f7d2ebcddd9c6a43047b0dda2eb5
sewonkimm <swon962@gmail.com>
2021-03-31 13:32:12 +0900

    refactor: 🍱 card container를 component로 분리

---
+++ frontend/src/components/main/cardContainer.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="cardContainer" id="cardContainer"></div>
+</template>
+<script>
+import { BookmarkGreen, Hot } from '@/assets/index.js';
+import CMRotate from '@/components/main/CMRotate.js';
+
+export default {
+  name: 'CardContainer',
+  data() {
+    return {
+      item: {
+        ingredientId: 3,
+        ingredientName: '배추',
+        ingredientNameCode: '211',
+        ingredientDetailName: null,
+        ingredientDetailNameCode: null,
+        ingredientCategory: '농산',
+        ingredientAvg: {
+          ingredientId: 3,
+          ingredientAvgDate: '2021-03-19',
+          ingredientAvgPrice: 3794,
+          ingredientAvgPredictPrice: 0,
+        },
+        bookmark: BookmarkGreen,
+        status: Hot,
+      },
+    };
+  },
+  methods: {
+    makeCardList(num) {
+      const cardList = [];
+      for (let index = 0; index < num; index++) {
+        cardList.push(this.item);
+      }
+      return cardList;
+    },
+  },
+  created() {
+    // this.$axios({
+    //   url: '/ingredients/',
+    //   method: 'GET',
+    // })
+    //   .then((response) => {
+    //     this.item = JSON.parse(response.data);
+    //     console.log(JSON.parse(response.data));
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+  },
+  mounted() {
+    const cardList = this.makeCardList(83);
+    const clickCard = (no) => {
+      alert('click no - ' + (no + 1));
+    };
+    const radius = window.innerHeight >= 900 ? 1400 : 1200;
+    CMRotate.init('cardContainer', 240, 320, 700, 8, radius, cardList, clickCard);
+  },
+};
+</script>
--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -22,53 +22,20 @@
     </v-toolbar>
 
     <!-- 카드 컴포넌트 -->
-    <div class="cardContainer" id="cardContainer"></div>
+    <CardContainer />
   </div>
 </template>
 <script>
 import '@/components/css/main/style.scss';
 import '@/components/css/main/card.scss';
 import { Logo } from '@/assets/index.js';
-// import CMRotate from '@/components/main/CMRotate.js';
+import CardContainer from '@/components/main/cardContainer';
 
 export default {
   name: 'Main',
   components: {
     Logo,
-  },
-  data() {
-    return {
-      item: [],
-    };
-  },
-  methods: {
-    makeCardList(num) {
-      const cardList = [];
-      for (let index = 0; index < num; index++) {
-        cardList.push(this.item);
-      }
-      return cardList;
-    },
-  },
-  created() {
-    this.$axios({
-      url: '/ingredients',
-      method: 'GET',
-    })
-      .then((response) => {
-        this.item = response.data;
-      })
-      .catch(() => {});
-  },
-  mounted() {
-    console.log('mounted');
-    console.log(this.item);
-    // const cardList = this.makeCardList(83);
-    // const clickCard = (no) => {
-    //   alert('click no - ' + (no + 1));
-    // };
-    // const radius = window.innerHeight >= 900 ? 1400 : 1200;
-    // CMRotate.init('cardContainer', 240, 320, 700, 8, radius, this.item, clickCard);
+    CardContainer,
   },
 };
 </script>
