f486721569cf651cf7cd848d6133447875d8e18b
김세원 <swon962@gmail.com>
2021-03-31 08:06:27 +0000

    #S04P23A301-87 메인페이지 농수축산물 조회 api 연결

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
+import axios from 'axios';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$axios = axios;
+axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 new Vue({
   router,
--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -22,56 +22,20 @@
     </v-toolbar>
 
     <!-- 카드 컴포넌트 -->
-    <div class="cardContainer" id="cardContainer"></div>
+    <CardContainer />
   </div>
 </template>
 <script>
 import '@/components/css/main/style.scss';
 import '@/components/css/main/card.scss';
-import { Logo, Hot, BookmarkGreen } from '@/assets/index.js';
-import CMRotate from '@/components/main/CMRotate.js';
+import { Logo } from '@/assets/index.js';
+import CardContainer from '@/components/main/cardContainer';
 
 export default {
   name: 'Main',
   components: {
     Logo,
-  },
-  data() {
-    return {
-      item: {
-        ingredientId: 3,
-        ingredientName: '배추',
-        ingredientNameCode: '211',
-        ingredientDetailName: null,
-        ingredientDetailNameCode: null,
-        ingredientCategory: '농산',
-        ingredientAvg: {
-          ingredientId: 3,
-          ingredientAvgDate: '2021-03-19',
-          ingredientAvgPrice: 3794,
-          ingredientAvgPredictPrice: 0,
-        },
-        bookmark: BookmarkGreen,
-        status: Hot,
-      },
-    };
-  },
-  methods: {
-    makeCardList(num) {
-      const cardList = [];
-      for (let index = 0; index < num; index++) {
-        cardList.push(this.item);
-      }
-      return cardList;
-    },
-  },
-  mounted() {
-    const cardList = this.makeCardList(83);
-    const clickCard = (no) => {
-      alert('click no - ' + (no + 1));
-    };
-    const radius = window.innerHeight >= 900 ? 1400 : 1200;
-    CMRotate.init('cardContainer', 240, 320, 700, 8, radius, cardList, clickCard);
+    CardContainer,
   },
 };
 </script>
