498c70f607211a0eedc1fab78cb77d677c3ba7f4
ingorithm <ingu94@gmail.com>
2021-04-01 11:35:50 +0900

    fix: 🐛 #S04P23A301-92 SQL작성 (전체조회_비로그인)
    ✅ Closes: #S04P23A301-92

--- sql/전체조회(비로그인).sql
+++ sql/전체조회(비로그인).sql
@@ -7,16 +7,22 @@
         ingredient_avg.ingredient_avg_price,
         ingredient_avg.ingredient_avg_date,
         ingredient_avg.ingredient_avg_predict_price,
-        count(watch.ingredient_id) AS 'watch'
+        (
+			SELECT count(*)
+			FROM watch
+			WHERE ingredient.ingredient_id = watch.ingredient_id
+		) AS "count"
         
-        FROM ingredient, ingredient_avg, watch
-
-        WHERE ingredient.ingredient_id = ingredient_avg.ingredient_id AND
-        watch.ingredient_id = ingredient.ingredient_id AND
-        ingredient_avg.ingredient_avg_date = "2019-01-01" AND
+        FROM ingredient LEFT OUTER JOIN ingredient_avg ON ingredient.ingredient_id = ingredient_avg.ingredient_id, watch
+		
+        WHERE ingredient_avg.ingredient_avg_date = "2019-01-01" AND
         watch.watch_date = "2019-01-01"
         
-        GROUP BY watch.ingredient_id
+        GROUP BY ingredient.ingredient_id
         
-        ORDER BY count(watch.ingredient_id) DESC;
+        ORDER BY (
+			SELECT count(*)
+			FROM watch
+			WHERE ingredient.ingredient_id = watch.ingredient_id
+		) DESC;
         
