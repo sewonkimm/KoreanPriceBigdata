548c673c37965f6e8c1872ab73f5a5941ae87bd6
김세원 <swon962@gmail.com>
2021-04-01 04:53:39 +0000

    #S04P23A301-89 로그인 페이지 마크업

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,4 +1,7 @@
 import Logo from './logo.svg';
+import LogoName from './logoWname.svg';
+import Google from './google.svg';
+import Kakao from './kakao.svg';
 import BookmarkRed from './bookmark_red.png';
 import BookmarkGreen from './bookmark_green.png';
 import BookmarkBlue from './bookmark_blue.png';
@@ -6,4 +9,15 @@ import Hot from './hot.png';
 import Warning from './warning.png';
 import Login from './login.png';
 
-export { Logo, BookmarkRed, BookmarkGreen, BookmarkBlue, Hot, Warning, Login };
+export {
+  Logo,
+  LogoName,
+  Google,
+  Kakao,
+  BookmarkRed,
+  BookmarkGreen,
+  BookmarkBlue,
+  Hot,
+  Warning,
+  Login,
+};
---
+++ frontend/src/components/login/socialLogin.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="socialLogin">
+    <v-dialog v-model="dialog" width="320">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn class="socialLoginButton" height="63" v-bind="attrs" v-on="on">
+          다른 방법으로 시작하기
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="title">
+          다른 방법으로 시작하기
+        </v-card-title>
+
+        <div class="socialLoginButton">
+          <Google />
+          <Kakao />
+        </div>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="dialog = false" class="closeButton">
+            닫기
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script>
+import { Google, Kakao } from '@/assets/index.js';
+
+export default {
+  name: 'SocialLogin',
+  components: {
+    Google,
+    Kakao,
+  },
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
