0b04a23736b9b06b32669a2e3e7f0001f468801e
sewonkimm <swon962@gmail.com>
2021-04-04 01:10:24 +0900

    feat: 🍎 검색 기능 기초 구현

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -18,7 +18,23 @@
 
     <!-- 검색창 -->
     <v-toolbar class="search">
-      <v-text-field hide-details prepend-icon="mdi-magnify" single-line></v-text-field>
+      <v-toolbar-title>
+        <v-icon>mdi-magnify</v-icon>
+      </v-toolbar-title>
+      <v-autocomplete
+        v-model="select"
+        :loading="loading"
+        :items="items"
+        :search-input.sync="search"
+        cache-items
+        class="mx-4"
+        flat
+        hide-no-data
+        hide-details
+        label="상품 검색"
+        solo
+      ></v-autocomplete>
+      <!-- <v-text-field hide-details prepend-icon="mdi-magnify" single-line></v-text-field> -->
     </v-toolbar>
 
     <!-- 카드 컴포넌트 -->
@@ -36,6 +52,102 @@ export default {
   components: {
     Logo,
     CardContainer,
+  },
+  data() {
+    return {
+      loading: false,
+      items: [],
+      search: null,
+      select: null,
+      ingredients: [
+        'Alabama',
+        'Alaska',
+        'American Samoa',
+        'Arizona',
+        'Arkansas',
+        'California',
+        'Colorado',
+        'Connecticut',
+        'Delaware',
+        'District of Columbia',
+        'Federated States of Micronesia',
+        'Florida',
+        'Georgia',
+        'Guam',
+        'Hawaii',
+        'Idaho',
+        'Illinois',
+        'Indiana',
+        'Iowa',
+        'Kansas',
+        'Kentucky',
+        'Louisiana',
+        'Maine',
+        'Marshall Islands',
+        'Maryland',
+        'Massachusetts',
+        'Michigan',
+        'Minnesota',
+        'Mississippi',
+        'Missouri',
+        'Montana',
+        'Nebraska',
+        'Nevada',
+        'New Hampshire',
+        'New Jersey',
+        'New Mexico',
+        'New York',
+        'North Carolina',
+        'North Dakota',
+        'Northern Mariana Islands',
+        'Ohio',
+        'Oklahoma',
+        'Oregon',
+        'Palau',
+        'Pennsylvania',
+        'Puerto Rico',
+        'Rhode Island',
+        'South Carolina',
+        'South Dakota',
+        'Tennessee',
+        'Texas',
+        'Utah',
+        'Vermont',
+        'Virgin Island',
+        'Virginia',
+        'Washington',
+        'West Virginia',
+        'Wisconsin',
+        'Wyoming',
+      ],
+    };
+  },
+  watch: {
+    search(val) {
+      val && val !== this.select && this.querySelections(val);
+    },
+    select: function() {
+      // 검색한 카드 컴포넌트만 보여주기
+      console.log('select');
+    },
+  },
+  methods: {
+    querySelections(v) {
+      this.loading = true;
+      // Simulated ajax query
+      setTimeout(() => {
+        this.items = this.ingredients.filter((e) => {
+          return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1;
+        });
+        this.loading = false;
+      }, 500);
+    },
+    getIngredients() {
+      // ingredients 받아오기
+    },
+  },
+  created() {
+    this.getIngredients();
   },
 };
 </script>
