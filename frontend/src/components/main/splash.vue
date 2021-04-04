55370260f261bd6065b12607c6b743b0389a6d13
김세원 <swon962@gmail.com>
2021-04-04 06:28:56 +0000

    스플래시 화면과 온보딩 페이지 구현

---
+++ frontend/src/components/main/splash.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="splash">
+    <img src="@/assets/logoWname.png" class="logo" />
+  </div>
+</template>
+<script>
+import '@/components/css/main/splash.scss';
+
+export default {
+  name: 'Splash',
+};
+</script>
