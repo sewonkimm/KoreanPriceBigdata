d2c0a0a49f0ed9acab50cbdcfe0181fc4b7e51f1
chunawoos <77089164+chunawoos@users.noreply.github.com>
2021-04-05 17:03:48 +0900

    refactor: 🍱 member, 추천시스템 수정
    ✅ Closes: #S04P23A301-111

--- frontend/src/components/detail/cheapRecommend.vue
+++ frontend/src/components/detail/cheapRecommend.vue
@@ -48,6 +48,7 @@ export default {
         method: 'GET',
       })
         .then((response) => {
+          console.log(response.data);
           this.items = response.data;
         })
         .catch((error) => {
